3626e330fd966d8fbc7d96881cd0945c0b22b408
sapienlife2021 <84908083+sapienlife2021@users.noreply.github.com>
2022-05-23 13:29:07 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <ApplicationProvider {...eva} theme={eva.light}>
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+      </ApplicationProvider>
+    );
+  }
+}
